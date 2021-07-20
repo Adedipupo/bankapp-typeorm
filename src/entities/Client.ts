98b0afea1b7dfc2cacce94e33b687bcb6d6d0f21
Benedict <jokanolaadedipupo@gmail.com>
2021-07-20 03:08:39 +0100

    entities

--- src/entities/Client.ts
+++ src/entities/Client.ts
@@ -1,4 +1,13 @@
-import { BaseEntity, Column, Entity } from "typeorm";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToMany,
+  OneToMany,
+  Transaction,
+  UpdateDateColumn,
+} from "typeorm";
 
 @Entity("client")
 export class Client extends BaseEntity {
@@ -16,11 +25,37 @@ export class Client extends BaseEntity {
   })
   card_number: string;
   @Column({
-      type: "numeric"
+    type: "numeric",
   })
   balance: number;
   @Column({
-      default: true
+    default: true,
+    name: "active",
   })
   isActive: boolean;
+  @Column({
+    type: "simple-json",
+    nullable: true,
+  })
+  additional_info: {
+    age: number;
+    hair_color: string;
+  };
+
+  @Column({ type: "simple-array", default: [] })
+  family_members: string[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+//   @ManyToMany((type) => Banker, {
+//     cascade: true,
+//   })
+//   bankers: Banker[];
+
+//   @OneToMany(() => Transaction, (transaction) => transaction.client)
+//   transactions: Transaction[];
 }
