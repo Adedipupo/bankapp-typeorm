70836c011d3d0a0bd09423ae8340afa500c28666
Benedict <jokanolaadedipupo@gmail.com>
2021-07-20 09:24:11 +0100

    transaction route and controlller

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createTransaction } from "src/controllers/transaction";
+import { createTransaction } from "../controllers/transaction";
 
 const router = express.Router();
 
