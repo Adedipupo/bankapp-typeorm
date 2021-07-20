fb4a64fb4ac38ce1778eb122bc6190b30759205e
Benedict <jokanolaadedipupo@gmail.com>
2021-07-20 09:45:34 +0100

    banker-to-client route and controlller

---
+++ src/controllers/bankertoclient.ts
@@ -0,0 +1,36 @@
+import {Request,Response} from 'express';
+import { Client } from '../entities/Client';
+import { Banker } from '../entities/Banker';
+
+export const createTransaction = async(req:Request, res:Response)=>{
+    const {clientId} = req.params;
+    const {type,amount} = req.body;
+
+    const client = await Client.findOne(parseInt(clientId))
+
+    if(!client){
+        return res.status(400).json({
+            msg: 'Client not found'
+        })
+    }
+
+    const transaction = Transaction.create({
+        type,
+        amount,
+        client
+    })
+
+    await transaction.save()
+
+    if(type === TransactionTypes.DEPOSIT){
+        client.balance = client.balance + amount;
+    }else if(type === TransactionTypes.WITHDRAW){
+        client.balance = client.balance - amount
+    }
+    await client.save()
+
+    return res.status(201).json({
+        msg:'transaction added'
+    })
+
+}
