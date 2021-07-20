2aee5b86af9b16ade06235c349e111edbcc1be39
Benedict <jokanolaadedipupo@gmail.com>
2021-07-20 10:52:15 +0100

    query client

--- src/routes/client.ts
+++ src/routes/client.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { createClient, getAllClient } from "../controllers/client";
+import { createClient, getAllClient, queryClient } from "../controllers/client";
 import { deleteClient } from "../controllers/client";
 
 const router = express.Router();
 
 router.get("/",getAllClient)
+router.get("/q",queryClient)
 router.post("/create", createClient);
 router.delete("/delete/:clientId", deleteClient)
 export default router;
