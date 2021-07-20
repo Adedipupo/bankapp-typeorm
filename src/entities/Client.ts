bc5b98d37e667198ebe1cd3452bb3935d568f31f
Benedict <jokanolaadedipupo@gmail.com>
2021-07-20 03:02:35 +0100

    entities

--- src/entities/Client.ts
+++ src/entities/Client.ts
@@ -6,4 +6,13 @@ export class Client extends BaseEntity {
     first_name: string;
     @Column()
     last_name: string;
+    @Column({
+        unique:true
+    })
+    email:string;
+    @Column({
+        unique:true,
+        length: 10
+    })
+    card_number: string;
 }
