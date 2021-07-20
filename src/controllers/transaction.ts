db71d3075b0d1dd7a74a576000d0d87b7fbb383c
Benedict <jokanolaadedipupo@gmail.com>
2021-07-20 09:28:49 +0100

    transaction route and controlller

--- src/controllers/transaction.ts
+++ src/controllers/transaction.ts
@@ -1,12 +1,12 @@
 import {Request,Response} from 'express';
+import { Client } from 'src/entities/Client';
 import { Transaction } from '../entities/Transaction';
 
 
 export const createTransaction = async(req:Request, res:Response)=>{
+    const {clientId} = req.params;
+    const {type,amount} = req.body;
 
-    const transaction = Transaction.create({
-        
-    })
-    await transaction.save()
-    res.status(200).json({transaction})
+    const client = await Client.findOne(parseInt(clientId))
+    res.status(200).json({})
 }
