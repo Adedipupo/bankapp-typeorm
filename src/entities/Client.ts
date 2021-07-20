0d5345b3d51bb362cf0f7debb54a873b43f810d7
Benedict <jokanolaadedipupo@gmail.com>
2021-07-20 03:04:29 +0100

    entities

--- src/entities/Client.ts
+++ src/entities/Client.ts
@@ -1,18 +1,26 @@
 import { BaseEntity, Column, Entity } from "typeorm";
 
-@Entity('client')
+@Entity("client")
 export class Client extends BaseEntity {
-    @Column()
-    first_name: string;
-    @Column()
-    last_name: string;
-    @Column({
-        unique:true
-    })
-    email:string;
-    @Column({
-        unique:true,
-        length: 10
-    })
-    card_number: string;
+  @Column()
+  first_name: string;
+  @Column()
+  last_name: string;
+  @Column({
+    unique: true,
+  })
+  email: string;
+  @Column({
+    unique: true,
+    length: 10,
+  })
+  card_number: string;
+  @Column({
+      type: "numeric"
+  })
+  balance: number;
+  @Column({
+      default: true
+  })
+  isActive: boolean;
 }
