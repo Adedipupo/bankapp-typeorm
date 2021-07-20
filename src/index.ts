d68984f1781a4ce96e1560b77a4eb734db8dbb10
Benedict <jokanolaadedipupo@gmail.com>
2021-07-20 09:23:38 +0100

    transaction route and controlller

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { createConnection } from "typeorm";
 import clientRoutes  from './routes/client';
 import bankerRoutes  from './routes/banker';
+import transactionRoutes  from './routes/transaction';
 import { Banker } from "./entities/Banker";
 import { Client } from "./entities/Client";
 import { Transaction } from "./entities/Transaction";
@@ -25,6 +26,7 @@ const main = async () => {
     app.use(express.json());
     app.use('/client',clientRoutes)
     app.use('/banker',bankerRoutes)
+    app.use('/api',transactionRoutes)
     app.listen(8080,()=>{
         console.log(`Now runing on port ${port}`)
     })
