b627085c35612e9e8aa7c8df4dac64181dfeb95a
Benedict <jokanolaadedipupo@gmail.com>
2021-07-20 08:59:52 +0100

    createClient route and controlller

--- src/controllers/client.ts
+++ src/controllers/client.ts
@@ -1,8 +1,16 @@
 import {Request,Response} from 'express';
+import { Client } from 'src/entities/Client';
 
 
 export const createClient = async(req:Request, res:Response)=>{
-    const name = req.body;
+    const {firstName, lastName,email,cardNumber,balance} = req.body;
 
+    const client = Client.create({
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        card_number: cardNumber,
+        balance: balance,
+    })
     res.status(200).json({name})
 }
