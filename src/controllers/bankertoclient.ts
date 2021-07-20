899dcd2c2c83527d75882443b0392c0d7b1fb60d
Benedict <jokanolaadedipupo@gmail.com>
2021-07-20 09:53:10 +0100

    banker-to-client route and controlller

--- src/controllers/bankertoclient.ts
+++ src/controllers/bankertoclient.ts
@@ -3,5 +3,8 @@ import { Client } from '../entities/Client';
 import { Banker } from '../entities/Banker';
 
 export const createBankerToClient = async(req:Request, res:Response)=>{
-    
+    const {bankerId,clientId} = req.params;
+
+    const client = await Client.findOne(parseInt(clientId));
+    const banker = await Banker.findOne(parseInt(bankerId));
 }
