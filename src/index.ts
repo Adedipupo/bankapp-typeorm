645991a21fd6cfafb607fa8bc543da65f09f7e25
Benedict <jokanolaadedipupo@gmail.com>
2021-07-20 02:51:53 +0100

    createConnection

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,19 @@ import { createConnection } from "typeorm";
 
 
 const main = async() =>{
-    const connection = await createConnection({
-        type: "postgres",
-        host: "localhost",
-        port: 5431,
-        username: "macuser",
-        password: "1234",
-        database: "typeorm"
-    })
+    try {
+        await createConnection({
+            type: "postgres",
+            host: "localhost",
+            port: 5431,
+            username: "macuser",
+            password: "1234",
+            database: "typeorm"
+        })
+        console.log('Connected to postgres');
+    } catch (error) {
+        console.error('Unable to connect to postgres')
+    }
 }
 
 main();
