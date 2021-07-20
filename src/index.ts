3f6060e596b68c47d488e1bf480a1e6231cf8071
Benedict <jokanolaadedipupo@gmail.com>
2021-07-20 02:49:49 +0100

    createConnection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-
+import { createConnection } from "typeorm";
 
 
 
 const main = async() =>{
-
+    const connection = await createConnection({
+        type: "postgres",
+        host: "localhost",
+        port: 5431,
+        username: "macuser",
+        password: "1234",
+        database: "typeorm"
+    })
 }
 
 main();
