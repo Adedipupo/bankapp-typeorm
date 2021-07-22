b8ecb3e8823ab5a8bb4f5c5491e2924d1a84dbaa
Benedict <jokanolaadedipupo@gmail.com>
2021-07-22 06:47:50 +0100

    deploying to heroku

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,11 @@ const main = async () => {
   try {
     await createConnection({
       type: "postgres",
-      host: "localhost",
-      port: 5431,
-      username: "macuser",
-      password: "1234",
-      database: "typeorm",
+      host: process.env.Host,
+      port: process.env.PORT as unknown as number,
+      username: process.env.Username,
+      password: process.env.Password,
+      database: process.env.Database,
       entities: [Client, Banker, Transaction],
       synchronize: true,
     });
